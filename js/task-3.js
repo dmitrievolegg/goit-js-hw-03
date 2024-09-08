1d187b842b179223e9c46f5e42e96d6126f8e383
dmitrievolegg <dmitriev.olegg@gmail.com>
2024-09-08 19:26:14 +0200

    Add homework 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,17 @@
-function getElementWidth(content, padding, border) {
-  // Перетворюємо значення з "Npx" в числа
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding) * 2; // Падінг на обидві сторони
-  const borderWidth = parseFloat(border) * 2; // Бордер на обидві сторони
+function filterArray(numbers, value) {
+  const newArray = [];
 
-  // Загальна ширина в форматі border-box
-  return contentWidth + paddingWidth + borderWidth;
+  for (let number of numbers) {
+    if (number > value) {
+      newArray.push(number);
+    }
+  }
+
+  return newArray;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
